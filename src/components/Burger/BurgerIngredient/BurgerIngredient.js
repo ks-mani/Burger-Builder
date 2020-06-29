91071ab440daabd019192561a88e64e61f2b27ce
ksmani <kumar.mani017@gmail.com>
2020-06-29 22:04:32 +0530

    08-129 => Starting the Burger Component

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './BurgerIngredient.module.css';
 import PropTypes from 'prop-types';
 
-class burgerIngredient extends React.Component{
+class BurgerIngredient extends React.Component{
 
   render(){
     let ingredient = null;
@@ -14,22 +14,22 @@ class burgerIngredient extends React.Component{
       case ('bread-top') :
         ingredient = (
           <div className={classes.BreadTop}>
-            <div className={classes.Seed1}></div>
-            <div className={classes.Seed2}></div>
+            <div className={classes.Seeds1}></div>
+            <div className={classes.Seeds2}></div>
           </div>
         );
         break;
       case ('meat') :
-        ingredient = <div className="classes.Meat"></div>;
+        ingredient = <div className={classes.Meat}></div>;
         break;
       case ('cheese') :
-        ingredient = <div className="classes.Cheese"></div>;
+        ingredient = <div className={classes.Cheese}></div>;
         break;
       case ('salad') :
-        ingredient = <div className="classes.Salad"></div>;
+        ingredient = <div className={classes.Salad}></div>;
         break;
       case ('bacon') :
-        ingredient = <div className="classes.Bacon"></div>;
+        ingredient = <div className={classes.Bacon}></div>;
         break;
       default:
         ingredient=null;
@@ -42,7 +42,7 @@ class burgerIngredient extends React.Component{
 
 
 BurgerIngredient.propTypes = {
-  type: PropTypes.string.isRequired;
+  type: PropTypes.string.isRequired
 }
 
-export default burgerIngredient;
+export default BurgerIngredient;
