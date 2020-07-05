fd23b2e488b86ec44f0e8f95669ca6c241423bcd
ksmani <kumar.mani017@gmail.com>
2020-07-05 15:55:38 +0530

    12-225 => Navigating back and to next page

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -12,10 +12,20 @@ class Checkout extends React.Component {
     }
   }
 
+  checkoutCancelledHandler = ()=>{
+    this.props.history.goBack();
+  }
+  checkoutContinuedHandler = ()=>{
+    this.props.history.replace('/checkout/contact-data');
+  }
+
   render(){
     return (
       <div>
-        <CheckoutSummary ingredients={this.state.ingredients}/>
+        <CheckoutSummary
+            ingredients={this.state.ingredients}
+            onCheckoutCancelled={this.checkoutCancelledHandler}
+            onCheckoutContinued= {this.checkoutContinuedHandler} />
       </div>
     )
   }
