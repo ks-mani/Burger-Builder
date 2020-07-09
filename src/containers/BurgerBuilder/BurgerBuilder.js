4c22f19259d97025ce483eedefcb6bf70210e806
ksmani <kumar.mani017@gmail.com>
2020-07-09 19:25:25 +0530

    17-306 => Initializing ingredients in the burger builder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,6 +18,8 @@ class BurgerBuilder extends Component{
   }
 
   componentDidMount(){
+    console.log(this.props);
+    this.props.onInitIngredients();
     // axios.get('https://react-my-burger-c533c.firebaseio.com/ingredients.json')
     //   .then(response=>{
     //     this.setState({ingredients: response.data});
@@ -141,14 +143,17 @@ class BurgerBuilder extends Component{
 const mapStateToProps = (state)=>{
   return {
     ings: state.ingredients,
-    price: state.totalPrice
+    price: state.totalPrice,
+    error: state.error
   }
 }
 
 const mapDispatchToProps = (dispatch)=>{
   return {
     onIngredientAdded: (ingName)=>dispatch(burgerBuilderActions.addIngredient(ingName)),
-    onIngredientRemoved: (ingName)=>dispatch(burgerBuilderActions.removeIngredient(ingName))
+    onIngredientRemoved: (ingName)=>dispatch(burgerBuilderActions.removeIngredient(ingName)),
+    onInitIngredients: ()=>dispatch(burgerBuilderActions.initIngredients())
+
   }
 }
 
--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -4,7 +4,7 @@ const initialState = {
   ingredients: null,
   totalPrice: 4,
   error: false,
-  
+
 }
 
 const INGREDIENT_PRICES = {
@@ -33,6 +33,17 @@ const reducer = (state=initialState,action)=>{
           [action.ingredientName]: state.ingredients[action.ingredientName]-1
         },
         totalPrice: state.totalPrice-INGREDIENT_PRICES[action.ingredientName]
+      }
+    case actionTypes.SET_INGREDIENTS:
+      return {
+        ...state,
+        ingredients: action.ingredients,
+        error: false
+      }
+    case action.FETCH_INGREDIENTS_FAILED:
+      return {
+        ...state,
+        error: true
       }
     default:
       return state;
