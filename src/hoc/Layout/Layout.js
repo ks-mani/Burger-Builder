4810c2ae69c69f42264cc4327863842c1c8b8ba2
ksmani <kumar.mani017@gmail.com>
2020-07-11 01:32:33 +0530

    18-332 => Updating the UI depending on Auth State

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Aux from "../Aux/Aux";
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
-
+import {connect} from 'react-redux'
 import classes from './Layout.module.css'
 
 class Layout extends React.Component {
@@ -23,8 +23,12 @@ class Layout extends React.Component {
   render(){
     return (
       <Aux>
-        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
-        <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+        <Toolbar
+          isAuth={this.props.isAuthenticated}
+          drawerToggleClicked={this.sideDrawerToggleHandler}/>
+        <SideDrawer
+          isAuth={this.props.isAuthenticated}
+          open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
         <main className={classes.Content}>
           {this.props.children}
         </main>
@@ -33,4 +37,10 @@ class Layout extends React.Component {
   }
 }
 
-export default Layout;
+const mapStateToProps = state=>{
+  return {
+    isAuthenticated: state.auth.token!==null
+  }
+}
+
+export default connect(mapStateToProps)(Layout);
