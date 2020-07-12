e65fd7efdf41a98823ab99ee2a24bd905613b827
ksmani <kumar.mani017@gmail.com>
2020-07-12 22:59:15 +0530

    19-346 => Sharing the validation method

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -9,7 +9,7 @@ import Input from '../../../components/UI/Input/Input';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler'
 import * as actions from '../../../store/actions/index'
 
-import {updateObject} from '../../../shared/utility';
+import {updateObject, checkValidity} from '../../../shared/utility';
 
 class ContactData extends React.Component {
 
@@ -127,7 +127,7 @@ class ContactData extends React.Component {
   inputChangedHandler = (event, inputIdentifier)=>{
     const updatedFormElement= updateObject(this.state.orderForm[inputIdentifier], {
       value: event.target.value,
-      valid: this.checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
+      valid: checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
       touched: true
     })
     const updatedOrderForm = updateObject(this.state.orderForm, {
@@ -141,21 +141,6 @@ class ContactData extends React.Component {
 
     // console.log(updatedFormElement);
     this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid})
-  }
-
-  checkValidity(value, rules){
-    if(!rules) return true;
-    let isValid = true;
-    if(rules.required){
-      isValid = value.trim()!=='' && isValid;
-    }
-    if(rules.minLength){
-      isValid = value.length>=rules.minLength && isValid;
-    }
-    if(rules.maxLength){
-      isValid = value.length<=rules.maxLength && isValid;
-    }
-    return isValid;
   }
 
   render(){
