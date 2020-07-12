bc7b6c27f75404fae49ec43bcb8a39f358bfe977
ksmani <kumar.mani017@gmail.com>
2020-07-12 14:48:16 +0530

    18-336 => Persistent Auth State with local storage

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders'
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
+import {connect} from 'react-redux'
+import * as actions from './store/actions/index'
 
 import {Route, Switch} from 'react-router-dom';
 
 class App extends Component {
+  componentDidMount(){
+    this.props.onTryAutoSignup();
+  }
   render(){
     return (
       <div>
@@ -26,4 +31,10 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch=>{
+    return {
+      onTryAutoSignup: ()=>dispatch(actions.authCheckState())
+    }
+}
+
+export default connect(null, mapDispatchToProps)(App);
