6cc48f6a4d02f153dd1014461a14a81b76f38d42
ksmani <kumar.mani017@gmail.com>
2020-07-12 16:02:21 +0530

    18-340 => Displaying User Specific Orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -23,7 +23,7 @@ class Orders extends React.Component {
     //   .catch(err=>{
     //     this.setState({loading: false});
     //   })
-    this.props.onFetchOrders(this.props.token);
+    this.props.onFetchOrders(this.props.token, this.props.userId);
   }
 
   render(){
@@ -48,13 +48,14 @@ const mapStateToProps = state => {
   return {
     orders: state.order.order,
     loading: state.order.loading,
-    token: state.auth.token
+    token: state.auth.token,
+    userId: state.auth.userId
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    onFetchOrders : (token)=>dispatch(actions.fetchOrders(token))
+    onFetchOrders : (token, userId)=>dispatch(actions.fetchOrders(token, userId))
   }
 }
 
