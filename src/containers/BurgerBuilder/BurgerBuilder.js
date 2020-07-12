9f0f36515f19c5c1d69b2878bed475393171db67
ksmani <kumar.mani017@gmail.com>
2020-07-13 00:43:58 +0530

    20-360 => Testing Containers

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -12,7 +12,7 @@ import * as burgerBuilderActions from '../../store/actions/index';
 import axios from '../../axios-order';
 
 
-class BurgerBuilder extends Component{
+export class BurgerBuilder extends Component{
   state = {
       purchasing: false
   }
