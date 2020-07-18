a3fe1507156530c1154c099e47d6d2567c986140
ksmani <kumar.mani017@gmail.com>
2020-07-18 15:50:09 +0530

    27-452 => Converting <App />

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useEffect} from 'react';
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
@@ -10,37 +10,36 @@ import * as actions from './store/actions/index'
 
 import {Route, Switch , withRouter, Redirect} from 'react-router-dom';
 
-class App extends Component {
-  componentDidMount(){
-    this.props.onTryAutoSignup();
-  }
-  render(){
-    let routes = (
+const App = (props) =>{
+  useEffect(()=>{
+    props.onTryAutoSignup();
+  }, []);
+
+  let routes = (
+    <Switch>
+      <Route path="/auth" component={Auth}/>
+      <Route path="/" exact component={BurgerBuilder}/>
+      <Redirect to="/" />
+    </Switch>
+  );
+  if(props.isAuthenticated){
+    routes = (
       <Switch>
+        <Route path="/checkout" component={Checkout}/>
+        <Route path="/orders" component={Orders}/>
+        <Route path="/logout" component={Logout}/>
         <Route path="/auth" component={Auth}/>
         <Route path="/" exact component={BurgerBuilder}/>
-        <Redirect to="/" />
       </Switch>
     );
-    if(this.props.isAuthenticated){
-      routes = (
-        <Switch>
-          <Route path="/checkout" component={Checkout}/>
-          <Route path="/orders" component={Orders}/>
-          <Route path="/logout" component={Logout}/>
-          <Route path="/auth" component={Auth}/>
-          <Route path="/" exact component={BurgerBuilder}/>
-        </Switch>
-      );
-    }
-    return (
-      <div>
-        <Layout>
-          {routes}
-        </Layout>
-      </div>
-    )
   }
+  return (
+    <div>
+      <Layout>
+        {routes}
+      </Layout>
+    </div>
+  )
 }
 
 const mapStateToProps = (state)=>{
