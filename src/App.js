281f72f71480639b2df2d5945061c74f518f6fda
ksmani <kumar.mani017@gmail.com>
2020-07-18 19:07:57 +0530

    27-462 => Setting the right useEffect dependencies

--- src/App.js
+++ src/App.js
@@ -22,9 +22,10 @@ const Auth = React.lazy(()=>{
 
 
 const App = (props) =>{
+  const {onTryAutoSignup}=props;
   useEffect(()=>{
     props.onTryAutoSignup();
-  }, []);
+  }, [onTryAutoSignup]);
 
   let routes = (
     <Switch>
